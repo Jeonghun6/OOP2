bf364c192cd817e4ca2fe776a54f0f0aa3072c56
12191835 <hajh1020@gmail.com>
2021-09-10 15:59:33 +0900

    asd

--- test1/test1/main.cpp
+++ test1/test1/main.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
-using namespace std;
+#include <string>
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+using std::getline;
+
+class GradeBook{
+public:
+	void displayMessage(string str) {
+		cout << "Welcome to the grade book for\n" << str << "!" << endl;
+	}
+};
 
 int main() {
-	cout << "Hello World" << endl;
-	cout << "Hello World" << endl;
+	string nameOfCourse;
+	GradeBook myGradeBook;
+	cout << "Please enter the course name : " << endl;
+	getline(cin, nameOfCourse);
+	myGradeBook.displayMessage(nameOfCourse);
 	return 0;
-
 }
